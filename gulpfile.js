f5db799d8d687e02ca7f10edbdf256283de995bd
Tomasz Borychowski <tomasz.borychowski@sap.com>
2017-11-15 23:06:58 +0000

    cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -5,37 +5,6 @@ const runElectron = require('gulp-run-electron');
 const notify = require('gulp-notify');
 const plumber = require('gulp-plumber');
 const sourcemaps = require('gulp-sourcemaps');
-// const gulpWebpack = require('webpack-stream');
-// const webpack = require('webpack');
-
-// const webpackConfig = {
-// 	devtool: 'eval',
-// 	watchOptions: { ignored: /node_modules/ },
-// 	entry: { index: './src/index.js' },
-// 	resolve: { extensions: ['.js', '.json'] },
-// 	output: {
-// 		libraryTarget: 'commonjs2',
-// 		filename: '[name].js'
-// 	},
-// 	module: {
-// 		// exprContextCritical: false,
-// 		rules: [{
-// 			test: /\.js$/,
-// 			exclude: /node_modules/,
-// 			use: { loader: 'babel-loader', options: { presets: ['electron'] } }
-// 		}]
-// 	},
-// 	target: 'electron',
-// 	externals: [
-// 		'fs',
-// 		'path',
-// 		'child_process',
-// 		'electron',
-// 		'electron-store',
-// 	],
-// 	// plugins: [ new webpack.optimize.UglifyJsPlugin() ]
-// };
-
 
 
 gulp.task('html', () => gulp
@@ -46,7 +15,6 @@ gulp.task('html', () => gulp
 
 gulp.task('js', () => gulp
 	.src('src/**/*.js')
-	// .pipe(gulpWebpack(webpackConfig))
 	.pipe(plumber({errorHandler: notify.onError('JS: <%= error.message %>')}))
 	.pipe(gulp.dest('app/'))
 );
